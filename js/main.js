cda0cc3523eeb29576042fac026053b8b99cef39
treexl <115030662+treexl3@users.noreply.github.com>
2023-12-17 12:45:55 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+onload = () => {
+        const wrapper = document.querySelector(".wrapper");
+        
+        document.body.classList.remove("container");
+
+        wrapper.classList.add("loaded");
+};
